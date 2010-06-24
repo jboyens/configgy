284e0eb56a05a790502f8adf6912a509c1c08cb0
JR Boyens <jr.boyens@chacha.com>
2010-06-24 15:40:36 -0400

    Fixed tests with new version of Specs

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -3,8 +3,10 @@ import com.twitter.sbt._
 
 
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
+
+  val scalaToolsSnapshots = ScalaToolsSnapshots
 
   override def pomExtra =
     <licenses>
